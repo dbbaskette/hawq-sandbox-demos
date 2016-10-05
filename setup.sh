67d082e689df0c1ae725949460b7d972acffc6f5
dbbaskette <dbbaskette@gmail.com>
2016-10-05 16:15:55 -0400

    su changes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 cd data
-sudo -c hdfs hadoop fs -mkdir /demo
+sudo hdfs hadoop fs -mkdir /demo
 for file in *.gz
 do
  tar xvfz $file
  dirName=${file/.tar.gz/}
- sudo -c hdfs hadoop fs -put $dirName /demo
+ sudo hdfs hadoop fs -put $dirName /demo
 done
-sudo -c hdfs hive -f hive.sql
+sudo hdfs hive -f hive.sql
 cp -R 2BW7K83GQ /usr/hdp/current/zeppelin-server/lib/notebook
 /usr/hdp/current/zeppelin-server/lib/bin/zeppelin-daemon.sh restart
 

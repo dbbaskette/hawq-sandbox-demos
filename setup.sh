9bca795c1c5c528a5b31cb556e874365b552c77f
dbbaskette <dbbaskette@gmail.com>
2016-10-05 16:23:40 -0400

    d

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ for file in *.gz
 do
  tar xvfz $file
  dirName=${file/.tar.gz/}
- sudo -u hdfs cd $currentDir/data;hadoop fs -put $dirName /demo
+ sudo -u hdfs bash -c cd $currentDir;hadoop fs -put $dirName /demo
 done
 sudo -u hdfs hive -f $currentDir/hive.sql
 cp -R ../2BW7K83GQ /usr/hdp/current/zeppelin-server/lib/notebook

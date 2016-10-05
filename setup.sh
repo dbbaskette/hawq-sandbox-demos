a94883efde6be854fc936ad3e2891df0e2cb93cf
dbbaskette <dbbaskette@gmail.com>
2016-10-05 16:50:41 -0400

    zep fix

--- setup.sh
+++ setup.sh
@@ -7,9 +7,8 @@ for file in *.gz
 do
  tar xvfz $file
  dirName=${file/.tar.gz/}
- sudo -u hdfs sh -c "cd $currentDir ; hadoop fs -put $dirName /demo"
+ sudo -u hdfs sh -c "cd $currentDir/data;hadoop fs -put $dirName /demo"
 done
 sudo -u hdfs hive -f $currentDir/hive.sql
-cp -R ../2BW7K83GQ /usr/hdp/current/zeppelin-server/lib/notebook
+sudo -u zeppelin sh -c "cp -R ../2BW7K83GQ /usr/hdp/current/zeppelin-server/lib/notebook"
 /usr/hdp/current/zeppelin-server/lib/bin/zeppelin-daemon.sh restart
-
